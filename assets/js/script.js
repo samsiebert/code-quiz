6a6e0206c94e73feff62f589ff0f00830016b465
samsiebert <sammsiebert@gmail.com>
2022-06-01 15:33:02 -0500

    call start button and event listener for click

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var startBtn = document.querySelector("#start-btn");
+var quiz = function() {
+    console.log("the button is working!");
+}
+
+//start quiz on click of start button
+startBtn.addEventListener("click", quiz);
